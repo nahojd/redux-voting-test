5069f9dc6934b353ddf7bc1e31caa41f6e552a1b
Johan Driessen <jd@approach.se>
2015-10-02 23:12:10 +0200

    Finished the server app. Created a store and a socket.io-server

---
+++ voting-server/index.js
@@ -0,0 +1,13 @@
+import makeStore from './src/store';
+import startServer from './src/server';
+
+export const store = makeStore();
+startServer(store);
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch({
+	type: 'SET_ENTRIES',
+	entries: require('./entries.json')
+});
+store.dispatch({ type: 'NEXT' });
